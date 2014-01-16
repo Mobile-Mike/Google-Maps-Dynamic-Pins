0654fd7b37551012f9cb33116bf919b91ca83524
Adrián Zúñiga Espinoza <phishingspam@outlook.com>
2014-01-16 14:44:52 -0600

    1.2

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+        <head>
+                <script type="text/javascript" src="js/mootools-core-1.4.5-full-nocompat.js" style=""></script>
+                <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=true"></script>
+                <script type="text/javascript" src="js/mapscript.js"></script>
+        </head>
+        <body>
+        <?php
+        $mapType = 'pins';
+        //$mapType = 'get_coords'; 
+        if($mapType == 'pins'): 
+        ?>
+        <div id="MapLocations">
+                <div class="location">
+                        <span class="id" style="display:none;">0</span>
+                        <span class="title" style="display:none;">Title</span>
+                        <span class="description" style="display:none;">Here goes a description</span>
+                        <span class="image"  style="display:none;">http://f.cl.ly/items/0Q3H3l29350W2r3U1c0Y/pusheen.gif</span>
+                        <span class="latitude" style="display:none;">20.672975</span>
+                        <span class="longitude" style="display:none;">-103.372986</span>
+                        <span class="date" style="display:none;">Jan 30 2014</span>
+                        <span class="icon" style="display:none;">your/icon/path</span>
+                </div>
+        </div>
+        <div id="map-canvas" style="height:300px"/>
+        <?php elseif($mapType == 'get_coords'): ?>
+                <input id="LocationLatitude" value="">
+                <input id="LocationLongitude" value="">
+                <div id="map-canvas" style="height:300px"/>
+        <?php endif; ?>
+        </body>
+</html>
--- js/mapscript.js
+++ js/mapscript.js
@@ -1,123 +1,123 @@
 window.addEvent('domready', function(){
-	var MapLocations = $('MapLocations');
-	var lat_lng = {
-		'latitude':$('LocationLatitude'),
-		'longitude':$('LocationLongitude')
-	}
-	if(MapLocations){
-		displayEvents();
-	}else if(lat_lng){
-		retrieveCoords()();
-	}
-	function displayEvents(){
-		var Locations = [];
-		MapLocations.getElements('.location').each(function(location, i){ 
-			var LocationDictionary = {
-				'id':location.getElement('.id').get('text'),
-				'title':location.getElement('.title').get('text'),
-				'description':location.getElement('.description').get('text'),
-				'latitude':location.getElement('.latitude').get('text'),
-				'longitude':location.getElement('.longitude').get('text'),
-				'date':location.getElement('.date').get('text'),
-				'image':location.getElement('.image').get('text')
-			};
-			Locations.push(LocationDictionary);
-		});
+        var MapLocations = $('MapLocations');
+        var lat_lng = {
+                'latitude':$('LocationLatitude'),
+                'longitude':$('LocationLongitude')
+        }
 
-		function initialize() {
-			var companyLocation = new google.maps.LatLng(20.672975,-103.362986);
-			var companyPin = {
-				url: 'icos/company_pin_icon.png'
-			}
-			var mapOptions = {
-				zoom: 16,
-				center: companyLocation,
-				mapTypeControlOptions: {mapTypeIds: [google.maps.MapTypeId.ROADMAP]},
-				mapTypeControl: false,
-				panControl: true,
-				zoomControl: true,
-				scaleControl: false,
-				streetViewControl: false
-			};
-			var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-			var companyMarker = new google.maps.Marker({
-				position: companyLocation,
-				map: map,
-				icon: companyPin,
-				title:"Place Company Name"
-			});
-			var infoWindows		= [];
-			var markers			= [];
-			Locations.each(function(location, i){
-				var infoWindow_Content = 
-					"<span><img src='/files/locations/image/" + location.image + "' " + "height='80px'></span>" +
-					"<div style='font-size: 20px; margin-bottom: 0px;'>" + location.title + "</div>" + 
-					"<p>" + location.description + "</p>" + 
-					"<p>" + location.date + "</p>";
+        if(MapLocations){
+                var Locations = [];
+                MapLocations.getElements('.location').each(function(location, i){ 
+                        var LocationDictionary = {
+                                'id':location.getElement('.id').get('text'),
+                                'title':location.getElement('.title').get('text'),
+                                'description':location.getElement('.description').get('text'),
+                                'latitude':location.getElement('.latitude').get('text'),
+                                'longitude':location.getElement('.longitude').get('text'),
+                                'date':location.getElement('.date').get('text'),
+                                'image':location.getElement('.image').get('text'),
+                                'icon':location.getElement('.icon').get('text')
+                        };
+                        Locations.push(LocationDictionary);
+                });
+                displayEvents();
+        }else if(lat_lng){
+                retrieveCoords();
+        }
 
-				markers[i] = new google.maps.Marker({
-						position: (new google.maps.LatLng(location.latitude,location.longitude)),
-						map: map,
-						icon: companyPin,
-						title:location.title
-				});
-				infoWindows[i] = new google.maps.InfoWindow({
-					content: infoWindow_Content,
-					minWidth: 300,
-					maxWidth: 300
-				});
-				new google.maps.event.addListener(markers[i], 'click', function(){
-					infoWindows[i].open(map, markers[i]);
-				});
-				
-			});
-		};
-		google.maps.event.addDomListener(window, 'load', initialize);
-	}
+        function displayEvents(){
+                function initialize() {
+                        var companyLocation = new google.maps.LatLng(20.672975,-103.362986);
+                        // if(Locations.icon == ''){
+                        //         var companyPin = {url: Locations.icon}       
+                        // }
 
-	function retrieveCoords() {
-		function initialize() {
-			var companyLocation = new google.maps.LatLng(20.672975,-103.362986);
-			var companyPin = {
-				url: '/icos/company_pin_icon.png'
-			}
-			var mapOptions = {
-				zoom: 16,
-				center: companyLocation,
-				mapTypeControlOptions: {mapTypeIds: [google.maps.MapTypeId.ROADMAP]},
-				mapTypeControl: false,
-				panControl: true,
-				zoomControl: true,
-				scaleControl: false,
-				streetViewControl: false
-			};
-			var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+                        var mapOptions = {
+                                zoom: 16,
+                                center: companyLocation,
+                                mapTypeControlOptions: {mapTypeIds: [google.maps.MapTypeId.ROADMAP]},
+                                mapTypeControl: false,
+                                panControl: true,
+                                zoomControl: true,
+                                scaleControl: false,
+                                streetViewControl: false
+                        };
+                        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+                        var companyMarker = new google.maps.Marker({
+                                position: companyLocation,
+                                map: map,
+                                //icon: companyPin,
+                                title:"Place Company Name"
+                        });
+                        var infoWindows = [];
+                        var markers     = [];
+                        Locations.each(function(location, i){
+                                console.log(location.latitude);
+                                var infoWindow_Content = 
+                                        "<span><img src='" + location.image + "' " + "height='80px'></span>" +
+                                        "<div style='font-size: 20px; margin-bottom: 0px;'>" + location.title + "</div>" + 
+                                        "<p>" + location.description + "</p>" + 
+                                        "<p>" + location.date + "</p>";
 
-			//Get position from dragged pin
-			var contentString = 'Arrastra el pin al lugar deseado para obtener las coordenadas de éste.';
-			var infowindow = new google.maps.InfoWindow({
-				content: contentString,
-				minWidth: 300,
-				maxWidth: 300
-			});
-			var marker = new google.maps.Marker({
-				position: companyLocation,
-				map: map,
-				icon: companyPin,
-				title: 'lolsies for funsies',
-				draggable: true
-			});
-			google.maps.event.addListener(marker, 'click', function() {
-				infowindow.open(map,marker);
-			});
-			infowindow.open(map,marker);
+                                markers[i] = new google.maps.Marker({
+                                                position: (new google.maps.LatLng(location.latitude,location.longitude)),
+                                                map: map,
+                                                //icon: companyPin,
+                                                title:location.title
+                                });
+                                infoWindows[i] = new google.maps.InfoWindow({
+                                        content: infoWindow_Content,
+                                        minWidth: 300,
+                                        maxWidth: 300
+                                });
+                                new google.maps.event.addListener(markers[i], 'click', function(){
+                                        infoWindows[i].open(map, markers[i]);
+                                });
+                                
+                        });
+                };
+                google.maps.event.addDomListener(window, 'load', initialize);
+        }
 
-			//Put coordinates in their respective fields…
-			google.maps.event.addListener(marker, 'dragend', function(lat_lng) {
-				$('LocationLatitude').set('value', lat_lng.latLng.lat());
-				$('LocationLongitude').set('value', lat_lng.latLng.lng());
-			});
-		}
-		google.maps.event.addDomListener(window, 'load', initialize);
-	}
+        function retrieveCoords() {
+                function initialize() {
+                        var companyLocation = new google.maps.LatLng(20.672975,-103.362986);
+                        var mapOptions = {
+                                zoom: 16,
+                                center: companyLocation,
+                                mapTypeControlOptions: {mapTypeIds: [google.maps.MapTypeId.ROADMAP]},
+                                mapTypeControl: false,
+                                panControl: true,
+                                zoomControl: true,
+                                scaleControl: false,
+                                streetViewControl: false
+                        };
+                        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+                        //Get position from dragged pin
+                        var contentString = 'Arrastra el pin al lugar deseado para obtener las coordenadas de éste.';
+                        var infowindow = new google.maps.InfoWindow({
+                                content: contentString,
+                                minWidth: 300,
+                                maxWidth: 300
+                        });
+                        var marker = new google.maps.Marker({
+                                position: companyLocation,
+                                map: map,
+                                title: 'lolsies for funsies',
+                                draggable: true
+                        });
+                        google.maps.event.addListener(marker, 'click', function() {
+                                infowindow.open(map,marker);
+                        });
+                        infowindow.open(map,marker);
+
+                        //Put coordinates in their respective fields…
+                        google.maps.event.addListener(marker, 'dragend', function(lat_lng) {
+                                $('LocationLatitude').set('value', lat_lng.latLng.lat());
+                                $('LocationLongitude').set('value', lat_lng.latLng.lng());
+                        });
+                }
+                google.maps.event.addDomListener(window, 'load', initialize);
+        }
 });
